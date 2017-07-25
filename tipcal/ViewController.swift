2a645d015ea06699373c828d80bb5f5c769a2201
rebecca matthews <rebeccamatthews@rebeccas-macbook-pro.local>
2017-07-24 20:31:54 -0700

    more work not tipcal

--- tipcal/ViewController.swift
+++ tipcal/ViewController.swift
@@ -9,19 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var billAmountTextField: UITextField!
 
+    @IBOutlet weak var totalAmountLabel: UILabel!
+    @IBOutlet weak var amountLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func calculatedTip(_ sender: Any){
+        let billAmount = Double(billAmountTextField.text!) ?? 0
+        let tipAmount = billAmount * 0.2
+        let totalAmonunt = billAmount + tipAmount
+        
+        tipAmountLabel.text = String(format: "$%f", tipAmount)
+        totalAmountLabel.text = "$\(totalAmonunt)"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func onTap(_ sender: UITapGestureRecognizer) {
+    @IBAction func onTap(_ sender: UIGestureRecognizer){
+        
     }
 
 }
 
+ // optional concept
+   
